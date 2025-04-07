34acddf8c1355b7421887f4a01b133bbdc0a0843
Knolaisen <knolaisen@gmail.com>
2025-04-07 11:51:37 +0200

    refactor: merge the labels and image formating script

--- format.py
+++ format.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import pandas as pd
 
 # Define paths
 folder1 = "data/rbk/1_train-val_1min_aalesund_from_start/img1"
@@ -23,4 +24,33 @@ for filename in os.listdir(folder2):
     index += 1
     shutil.copy(src, dst)
 
-print("Merging complete!")
+
+path_1 = "data/rbk/1_train-val_1min_aalesund_from_start/gt/gt.txt"
+path_2 = "data/rbk/2_train-val_1min_after_goal/gt/gt.txt"
+labels_folder_path = "rbk_structured/train/labels/"
+
+# Read the files
+dataframe_1 = pd.read_csv(path_1, sep=",", header=None)
+dataframe_2 = pd.read_csv(path_2, sep=",", header=None)
+
+# Correct the indices
+dataframe_2[0] = dataframe_2[0] + 1802
+combined = pd.concat([dataframe_1, dataframe_2])
+
+# Rename the columns and drop unnecessary ones
+combined.rename(columns={0: "frame", 1: "object_number", 2: "x_center", 3: "y_center", 4: "width", 5: "height", 6: "off_screen", 7: "class", 8: "idk"}, inplace=True)
+combined.drop(columns=["off_screen", "idk"], inplace=True)
+
+# normalize the coordinates
+combined["x_center"] = combined["x_center"] / 1920
+combined["y_center"] = combined["y_center"] / 1080
+combined["width"] = combined["width"] / 1920
+combined["height"] = combined["height"] / 1080
+
+def save_to_file(dataframe, picture_id):
+    dataframe = dataframe[["class", "x_center", "y_center", "width", "height"]]
+    dataframe["class"] = dataframe["class"] - 1
+    dataframe.to_csv(f"data/rbk_structured/labels/train/{picture_id}.txt", header=False, index=False, sep=" ")
+
+for picture_id, dataframe in combined.groupby("frame"):
+    save_to_file(dataframe, picture_id)
